d74eae4284ec214a7650f703fe8af14a258a94ec
david <david@3aabc6e5-72f3-0310-b86c-b483baa2e007>
2005-07-22 14:44:05 +0000

    player - removed an unnecessary include from main.cpp - stdlib.h
    git-svn-id: svn://192.168.0.18/rr/instore/player_fc/trunk@319 3aabc6e5-72f3-0310-b86c-b483baa2e007

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
  ***************************************************************************/
 
 #include "player.h"
-#include <stdlib.h>
 #include "common/exception.h"
 #include "common/rr_misc.h"
 
