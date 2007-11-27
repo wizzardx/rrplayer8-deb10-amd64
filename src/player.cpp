wizzardx/rrplayer8-deb10-amd64

commit bededdb5d7971ae12d4349b023d95d7446bb979d

Implemented Stefans requested end-of-song transition changes
* Items now end sooner if they end quietly (or with silence)
  - They are stopped either when the song goes quiet, or when it starts going
    quiet (if rrmedia-maintenance determined that the song ends with a long
    fade out)
* Not overlapping songs when the current song ends suddenly
* If the current song fades out gradually, then we overlap songs for 1000 ms
  instead of whatever was configured in the wizard.
* Starting the next item after a 500 ms pause if the current item ends suddenly
* Songs which have a "built-in" fade (according to rrmedia-maintenance) are
  faded out for 2.5 seconds.
  - Up until the point when they go quiet. Then they are stopped.
* Other misc changes:
  - Logging a warning for songs which weren't dynamically range compressed
  - get_playback_events_info() now returns a description of the next event
  - Reformatted in a few places

src/player.cpp
@@ -1056,6 +1056,7 @@ void player::get_playback_events_info(playback_events_info & event_info, const i
   bool blnlinein_used = run_data.uses_linein(SU_CURRENT_FG);
   int intxmms_song_length_ms = -1;
   int intxmms_song_pos_ms = -1;
+  string stritem_ends_descr = "Item ends"; // Default description for the item end
 
   if (!blnlinein_used && run_data.current_item.cat != SCAT_SILENCE) {
     // XMMS is being used to play this item.
@@ -1063,7 +1064,56 @@ void player::get_playback_events_info(playback_events_info & event_info, const i
     intxmms_song_pos_ms    = xmmsc::xmms[intxmms_session].get_song_pos_ms();
     intxmms_song_length_ms = xmmsc::xmms[intxmms_session].get_song_length_ms();
 
-    event_info.intitem_ends_ms = intxmms_song_length_ms - intxmms_song_pos_ms;
+    // Default to assuming the item ends when XMMS gets to the end of the item:
+    int intend_ms = intxmms_song_length_ms;
+
+    // For songs we check the end more closely (if we have the info):
+    if (run_data.current_item.cat == SCAT_MUSIC &&
+        run_data.current_item.end.blnloaded) {
+      // Ending information is available, so we might end the song early.
+      // Check that XMMS's end matches the end recorded in the DB fairly closely:
+      int intdiff = abs(intxmms_song_length_ms - run_data.current_item.end.intlength_ms);
+      if (intdiff > 500) {
+        log_warning("XMMS says that " + run_data.current_item.strmedia + " is " + itostr(intxmms_song_length_ms) + " ms long, but the DB says it is " + itostr(run_data.current_item.end.intlength_ms) + " ms");
+      }
+
+      // Log a warning if the item is dynamically compressed:
+      if (!run_data.current_item.end.blndynamically_compressed) {
+        // Not dynamically range compressed
+        log_warning("It looks like " + run_data.current_item.strmedia + " is not dynamically range compressed, so I can't tell if it ends suddenly.");
+      }
+
+      // If the song becomes silent towards the end, then assume that the
+      // item ends there (until we decide it's okay to use the quiet end
+      // instead)
+      if (run_data.current_item.end.intend_silence_start_ms != -1 &&
+          run_data.current_item.end.intend_silence_start_ms < intend_ms) {
+        stritem_ends_descr = "Item goes silent";
+        intend_ms = run_data.current_item.end.intend_silence_start_ms;
+      }
+
+      // Now check if we end when the song starts getting quiet
+      // - Does the song fade out gradually?
+      if (run_data.current_item.end.blnends_with_fade) {
+        // In this case we stop the song when it starts getting quiet.
+        if (run_data.current_item.end.intend_quiet_start_ms != -1 &&
+            run_data.current_item.end.intend_quiet_start_ms < intend_ms) {
+          // We have a quiet start, and it is sooner than the ends we know
+          // about so far.
+          stritem_ends_descr = "Item goes quiet";
+          intend_ms = run_data.current_item.end.intend_quiet_start_ms;
+        }
+        else {
+          // The item fades out (according to rrmedia-maintenance), but we
+          // don't have a valid time for when it becomes quiet!
+          log_warning("Current item apparently fades out, but I don't know when it starts going quiet!");
+        }
+      }
+    }
+
+    // We now know where item playback should end:
+    event_info.intitem_ends_ms = intend_ms - intxmms_song_pos_ms;
+
     // Does this item have a music bed?
     if (run_data.current_item.blnmusic_bed) {
       // Music bed start:
@@ -1085,7 +1135,8 @@ void player::get_playback_events_info(playback_events_info & event_info, const i
   }
 
   // Is this item going to be interrupted to play promos? (ie, item is music, the segment allows promos, and there are waiting promos)
-  if (run_data.current_item.cat == SCAT_MUSIC && !run_data.next_item.blnloaded) {
+  if (run_data.current_item.cat == SCAT_MUSIC &&
+      !run_data.next_item.blnloaded) {
     // Current item is Music. Check if there are promos to play
 
     // Check if there is a promo that wants to play now (and is allowed to)
@@ -1183,15 +1234,21 @@ void player::get_playback_events_info(playback_events_info & event_info, const i
   // Find out when the "next" event is going to take place. This is simply the smallest
   // "ms" figure that has already been found.
   event_info.intnext_ms = event_info.intitem_ends_ms;
+  event_info.strnext_descr = stritem_ends_descr;
 
   // Define a macro which sets A to B if B is lower.
-  #define MY_SET_MIN(A,B) if (A > B) A = B
-  MY_SET_MIN(event_info.intnext_ms, event_info.intmusic_bed_starts_ms);
-  MY_SET_MIN(event_info.intnext_ms, event_info.intmusic_bed_ends_ms);
-  MY_SET_MIN(event_info.intnext_ms, event_info.intpromo_interrupt_ms);
-  MY_SET_MIN(event_info.intnext_ms, event_info.intrpls_interrupt_ms);
-  MY_SET_MIN(event_info.intnext_ms, event_info.inthour_change_interrupt_ms);
-  #undef MY_SET_MIN
+  #define COMPARE_NEXT_WITH(MS, DESCR) { \
+    if (MS < event_info.intitem_ends_ms) { \
+      event_info.intnext_ms = MS; \
+      event_info.strnext_descr = DESCR; \
+    } \
+  }
+  COMPARE_NEXT_WITH(event_info.intmusic_bed_starts_ms, "Music bed starts");
+  COMPARE_NEXT_WITH(event_info.intnext_ms, "Music bed ends");
+  COMPARE_NEXT_WITH(event_info.intnext_ms, "Interrupted by promo");
+  COMPARE_NEXT_WITH(event_info.intnext_ms, "Interrupted by playlist change");
+  COMPARE_NEXT_WITH(event_info.intnext_ms, "Interrupted by hour change");
+  #undef COMPARE_NEXT_WITH
 }
 
 int player::get_pe_vol(const string & strpe_vol) {

src/player.h
@@ -15,9 +15,10 @@
 class playback_events_info {
 public:
   playback_events_info(); ///< Constructor
-  void reset(); ///< Reset attributes back to default values.
-  int intnext_ms;      ///< ms until the next event happens
-  int intitem_ends_ms; ///< ms until the current item ends
+  void reset();           ///< Reset attributes back to default values.
+  int intnext_ms;         ///< ms until the next event happens
+  string strnext_descr;   ///< Short description of the next event
+  int intitem_ends_ms;    ///< ms until the current item ends
   int intmusic_bed_starts_ms; ///< ms until the music bed starts playing
   int intmusic_bed_ends_ms;   ///< ms until the music bed stops playing.
   int intpromo_interrupt_ms;  ///< ms until the item (ie, music) will be interrupted to play a promo.

src/player_playback_transition.cpp
@@ -149,6 +149,15 @@ void player::playback_transition(playback_events_info & playback_events) {
             run_data.next_item.cat == SCAT_MUSIC) && !blninterrupt) {
             log_line("HACK: One of the items is music, so crossfading");
             blncrossfade = true;
+            // Hack on top of that one: If the current item's ends suddenly
+            // (as determined by rrmedia-maintenance), then we don't
+            // crossfade. The next item will start immediately after the
+            // current item ends, instead.
+            if (run_data.current_item.end.blnloaded &&
+                !run_data.current_item.end.blnends_with_fade) {
+              log_line("HACK: Nevermind. The current item ends suddenly, so we won't crossfade");
+              blncrossfade = false;
+            }
         }
 
         // Log whether we are going to crossfade:
@@ -165,12 +174,38 @@ void player::playback_transition(playback_events_info & playback_events) {
         // Is the current item available?
         if (run_data.current_item.blnloaded) {
           // Yes. Are we going to crossfade?
-          if (blncrossfade)
+          if (blncrossfade) {
             // Yes. Next item starts just after [current item end] - [crossfade length]
-            intnext_item_start_ms = intitem_ends_ms - config.intcrossfade_length_ms + 1;
-          else
+            int intcrossfade_length = config.intcrossfade_length_ms;
+            // If the current item is a song, and it fades out gradually, then
+            // we should overlap for 1 second only (as per Stefan's request)
+            // instead of whatever the player is configured for
+            if (run_data.current_item.cat == SCAT_MUSIC &&
+                run_data.current_item.end.blnloaded &&
+                run_data.current_item.end.blnends_with_fade &&
+                config.intcrossfade_length_ms != 1000) {
+              log_message("The current item is a song which fades out gradually, so I will crossfade for 1000 ms instead of the configured " + itostr(config.intcrossfade_length_ms) + " ms");
+              intcrossfade_length = 1000;
+            }
+            intnext_item_start_ms = intitem_ends_ms - intcrossfade_length + 1;
+          }
+          else {
             // No. Next item starts just after the current item ends
-            intnext_item_start_ms = intitem_ends_ms + 1;
+
+            // Is the current item a song which ends suddenly?
+            if (run_data.current_item.cat == SCAT_MUSIC &&
+                run_data.current_item.end.blnloaded &&
+                !run_data.current_item.end.blnends_with_fade &&
+                run_data.current_item.end.blndynamically_compressed) {
+              // Yes. Start the next song in half a second, as requested by Stefan:
+              log_message("Current song ends suddenly, so the next item will start 500 ms after it ends");
+              intnext_item_start_ms = intitem_ends_ms + 500;
+            }
+            else {
+              // Nope. Start the next song immediately:
+              intnext_item_start_ms = intitem_ends_ms + 1;
+            }
+          }
         }
         else {
           // Current item is not available. Next item starts immediately.
@@ -203,7 +238,20 @@ void player::playback_transition(playback_events_info & playback_events) {
             if (!blncrossfade) log_message("The current item will fade out");
             blnfade = true; // This transition includes a fade
           }
-           
+
+          // Another hack: If the item is music and fades out by itself over a
+          // longish period, then it will be too quiet to hear over the last part
+          // so we need to fade it out earlier.
+          if (!blnfade &&
+              run_data.current_item.cat == SCAT_MUSIC &&
+              run_data.current_item.end.blnloaded &&
+              run_data.current_item.end.blnends_with_fade) {
+            log_line("HACK: Nevermind. It looks like the current item is a song with a long, drawn-out fade, so we will fade it out for 2.5s just before it starts going quiet");
+            queue_volslide(events, "current", 100, 0, intitem_ends_ms - 2500, 2500);
+            if (!blncrossfade) log_message("The current item will fade out");
+            blnfade = true; // This transition includes a fade
+          }
+
 //          queue_volslide(events, "current", 100, 0, intitem_ends_ms - config.intcrossfade_length_ms, config.intcrossfade_length_ms);
 //          if (!blncrossfade) log_message("The current item will fade out");
 //          blnfade = true; // This transition includes a fade

src/programming_element.cpp
@@ -74,6 +74,11 @@ void programming_element::load_end(pg_connection & db) {
   end.intend_quiet_start_ms = strtoi(rs.field("intend_quiet_start_ms", "-1"));
   end.blnends_with_fade = strtobool(rs.field("blnends_with_fade", "f"));
   end.blnloaded = true;
+
+  // Log a warning at this point if the mp3 is a song and not dynamically compressed;
+  if (cat == SCAT_MUSIC && !end.blndynamically_compressed) {
+    log_warning("Song " + strmedia + " is not dynamically range compressed! I can't tell if it gradually fades out.");
+  }
 }
 
 // A global variable containing the previous music segment's programming element list

src/programming_element.h
@@ -61,7 +61,7 @@ public:
                                     //   volume-related guesses about the end
                                     //   of the song
     int intend_quiet_start_ms; // When does the end of the song start to go quiet/fade?
-    bool blnends_with_fade; // Does the item end with a drawn-out fade?
+    bool blnends_with_fade; // Does the item end with a drawn-out fade? (otherwise, it ends suddenly)
   } end;
   void load_end(pg_connection & db);
 };

commit 42beadb5e928340fdfff1c5f39153948d1a034dc

Fixed a bug calculating the time of the next event.
Was always assuming the next item occurred when the current item stopped. Never
due to promo interruptions, etc.

This bug was introduced in commit bededdb5

src/player.cpp
@@ -1267,9 +1267,9 @@ void player::get_playback_events_info(playback_events_info & event_info, const i
   }
   COMPARE_NEXT_WITH(event_info.intmusic_bed_starts_ms, "Music bed starts");
   COMPARE_NEXT_WITH(event_info.intnext_ms, "Music bed ends");
-  COMPARE_NEXT_WITH(event_info.intnext_ms, "Interrupted by promo");
-  COMPARE_NEXT_WITH(event_info.intnext_ms, "Interrupted by playlist change");
-  COMPARE_NEXT_WITH(event_info.intnext_ms, "Interrupted by hour change");
+  COMPARE_NEXT_WITH(event_info.intpromo_interrupt_ms, "Interrupted by promo");
+  COMPARE_NEXT_WITH(event_info.intrpls_interrupt_ms, "Interrupted by playlist change");
+  COMPARE_NEXT_WITH(event_info.inthour_change_interrupt_ms, "Interrupted by hour change");
   #undef COMPARE_NEXT_WITH
 }
 