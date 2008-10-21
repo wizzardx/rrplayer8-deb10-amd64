7a5088773ee408891f62010888351976b99a21c2
David Purdy <david@radioretail.co.za>
2008-10-21 12:29:43 +0200

    Tidy get_next_ok_music_item() test
    More more logic to setUp() and tearDown().

--- tests/test_player_get_next_item.h
+++ tests/test_player_get_next_item.h
@@ -8,29 +8,32 @@
 #include "player.h"
 #include "common/my_string.h"
 
+// Helper types
+
+typedef auto_ptr<pg_transaction> ap_pg_transaction;
+
 // Tests for get_next_ok_music_item()
 
 class TestGetNextOkMusicItem : public CxxTest::TestSuite
 {
 public:
-
-    music_history mhistory;
-    mp3_tags mp3tags;
-    pg_connection db;
-    player_config config;
-    player_run_data run_data;
+    // Main objects passed to get_next_ok_music_item()
+    int intstarts_ms;
     programming_element next_item;
 
-    // Constructor
-    TestGetNextOkMusicItem() {
-        // Connect to database
-        db.open("dbname=schedule_test");
-    }
+    // Other objects passed to get_next_ok_music_item(), providing data it
+    // needs to operate
+    music_history mhistory;
+    mp3_tags mp3tags;
+    player_config config;
+    player_run_data run_data;
+    ap_pg_transaction trans;
 
     // Per-test fixture setup
     void setUp()
     {
-        // Setup objects
+        // Setup objects to be passed to get_next_ok_music_item()
+        intstarts_ms = 5000;
         programming_element pe;
         pe.cat = SCAT_MUSIC;
         pe.strmedia = "/dir/to/music/mp3s/test.mp3";
@@ -38,27 +41,31 @@ public:
         run_data.current_segment.programming_elements.clear();
         run_data.current_segment.programming_elements.push_back(pe);
         run_data.current_segment.blnloaded = true;
+
+        // Setup a database transaction, with testing data
+        pg_connection db;
+        db.open("dbname=schedule_test");
+        trans = ap_pg_transaction(new pg_transaction(db));
+        _setup_test_db_data(*trans);
+    }
+
+    // Per-test fixture teardown
+    void tearDown()
+    {
+        trans->abort();
     }
 
     // A basic "smoke test" for get_next_ok_music_item()
     void test_should_work()
     {
-        // Make a DB transaction and put some test data in the database
-        pg_transaction trans(db);
-        _setup_test_db_data(trans);
-
         // Run the tested function
-        int intstarts_ms = 5000;
         get_next_ok_music_item(next_item, intstarts_ms, mhistory, mp3tags,
-                               trans, config, run_data);
+                               *trans, config, run_data);
 
         // Check the output of the tested function
         TS_ASSERT(next_item.blnloaded);
         TS_ASSERT_EQUALS(next_item.cat, SCAT_MUSIC);
         TS_ASSERT_EQUALS(next_item.strmedia, "/dir/to/music/mp3s/test.mp3");
-
-        // Rollback the DB transaction
-        trans.abort();
     }
 
     // Helper methods
